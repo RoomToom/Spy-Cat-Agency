c16a3e1bbab6fa7bbffac02a47efdd854846ce54
romtom <tokarchuk.roman@chnu.edu.ua>
2025-09-14 01:24:28 +0300

    commit: First tests pass

--- app/main.py
+++ app/main.py
@@ -3,8 +3,19 @@ from .api.routers.cats import router as cats_router
 from .api.routers.missions import router as missions_router
 from .api.routers.targets import router as targets_router
 from .core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Spy Cat Agency API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(cats_router, prefix=settings.API_V1_STR)
 app.include_router(missions_router, prefix=settings.API_V1_STR)
 app.include_router(targets_router, prefix=settings.API_V1_STR)
